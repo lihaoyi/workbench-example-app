8dc316a27be624bcf4acf9c21cafd0ff4e749645
Sébastien Doeraene <sjrdoeraene@gmail.com>
2013-06-19 18:52:29 +0200

    First minimal example.

---
+++ build.sbt
@@ -0,0 +1,5 @@
+scalaJSSettings
+
+name := "Example Scala.js application"
+
+version := "0.1-SNAPSHOT"
---
+++ src/main/scala/example/ScalaJSExample.scala
@@ -0,0 +1,7 @@
+package example
+
+object ScalaJSExample {
+  def main(): Unit = {
+    js.Dynamic.global.console.log("Yeah!")
+  }
+}
