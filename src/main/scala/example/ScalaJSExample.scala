0e6d72437a4c9d7109e390cff3b94d7f4cf27584
Sébastien Doeraene <sjrdoeraene@gmail.com>
2013-07-18 16:21:27 +0200

    The updateDynamic workaround is not needed anymore.

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -5,7 +5,7 @@ import js.Dynamic.{ global => g }
 object ScalaJSExample {
   def main(): Unit = {
     val paragraph = g.document.createElement("p")
-    paragraph.updateDynamic("innerHTML")("<strong>It works!</strong>")
+    paragraph.innerHTML = "<strong>It works!</strong>"
     g.document.getElementById("playground").appendChild(paragraph)
   }
 }
