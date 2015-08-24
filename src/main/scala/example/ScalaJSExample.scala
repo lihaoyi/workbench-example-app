d569e2e09a15a4c7cae2fb92e06112ed767dff62
Li Haoyi <haoyi@dropbox.com>
2015-08-24 15:33:37 -0400

    rainbowspiral

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,12 +1,16 @@
 package example
+
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
 import org.scalajs.dom.html
 import scala.util.Random
 
-case class Point(x: Int, y: Int){
-  def +(p: Point) = Point(x + p.x, y + p.y)
-  def /(d: Int) = Point(x / d, y / d)
+case class Vec2D(x: Double, y: Double){
+  def +(p: Vec2D) = Vec2D(x + p.x, y + p.y)
+  def -(p: Vec2D) = Vec2D(x - p.x, y - p.y)
+  def /(d: Double) = Vec2D(x / d, y / d)
+  def *(d: Double) = Vec2D(x * d, y * d)
 }
 
 @JSExport
@@ -15,30 +19,88 @@ object ScalaJSExample {
   def main(canvas: html.Canvas): Unit = {
     val ctx = canvas.getContext("2d")
                     .asInstanceOf[dom.CanvasRenderingContext2D]
+    ctx.fillStyle = "black"
+    canvas.width = 1000
+    canvas.height = 1000
+    val canvasHeight = 1000
+    ctx.fillRect(0, 0, canvasHeight, canvasHeight)
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
+    def drawLine(color: js.Any, pts: Vec2D*) = {
+      println(pts)
+      ctx.strokeStyle = color
+      ctx.fillStyle = color
 
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
+      ctx.beginPath()
+      ctx.moveTo(pts(0).x.toInt + canvasHeight/2, pts(0).y.toInt + canvasHeight/2)
+      for(pt <- pts) ctx.lineTo(pt.x.toInt + canvasHeight/2, pt.y.toInt + canvasHeight/2)
+      ctx.closePath()
+      ctx.fill()
+
     }
 
-    def run = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
+    val startCanvasRatio = 0.10
+    val segmentsPer360 = 20
+    val startAngle = -45
+    val totalAngle = 1440
+    val numSegments = segmentsPer360 * totalAngle / 360
+    val degreesPerSegment = 360 / segmentsPer360
+    val segmentGapFraction = 0.2
+    val growth = 1.5
+    val growthPerDegree = math.pow(growth, 1.0 / 360)
+    val shearRatio = 0.3
+    def offset(degrees: Double) = {
+      val radius = canvasHeight/2 * math.pow(growthPerDegree, degrees) * startCanvasRatio
+      val radians = (degrees + startAngle) * math.Pi / 180
+      Vec2D(
+        (math.cos(radians) * radius).toInt,
+        (math.sin(radians) * radius).toInt
+      )
     }
+    ctx.lineWidth = 6
 
-    dom.setInterval(() => run, 50)
+
+    def doLoop(color: (Int, Vec2D, Vec2D) => js.Any, myOffset: Int) = {
+      for(step <- 0 until numSegments){
+        def makePoints(degrees: Double) = {
+          val startPoint = offset(degrees)
+          val endPoint = offset(degrees + degreesPerSegment)
+          (startPoint, endPoint, (endPoint - startPoint) * segmentGapFraction)
+        }
+
+
+        throw new Exception("HALLOOOO")
+        val (start, end, gap) = makePoints(step * degreesPerSegment + myOffset)
+        val (start2, end2, gap2) = makePoints(step * degreesPerSegment + degreesPerSegment * shearRatio)
+
+        drawLine(
+          color(step, start, start2 * growth),
+          start,
+          end - gap,
+          (end2 - gap2) * growth,
+          start2 * growth
+        )
+      }
+    }
+    doLoop((_, _, _) => "grey", degreesPerSegment)
+    doLoop(
+      (step, p1, p2)=> {
+        val gradient = ctx.createLinearGradient(
+          p1.x.toInt + canvasHeight/2,
+          p1.y.toInt + canvasHeight/2,
+          p2.x.toInt + canvasHeight/2,
+          p2.y.toInt + canvasHeight/2
+        )
+        gradient.addColorStop(0, "black")
+        gradient.addColorStop(
+          1,
+          "hsl(" + (step * degreesPerSegment * 360 / totalAngle) + ", 50%, 50%)"
+        )
+        gradient
+      } ,
+
+
+      0
+    )
+
   }
 }
